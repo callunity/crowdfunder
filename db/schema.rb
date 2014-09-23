57877564ebc19f666e6839a8594014bef1df2ef7
callunity <heather.e.armstrong@gmail.com>
2014-09-23 15:38:38 -0400

    added Owner column to Project table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923190309) do
+ActiveRecord::Schema.define(version: 20140923193217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140923190309) do
     t.datetime "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
   end
 
   create_table "users", force: true do |t|
