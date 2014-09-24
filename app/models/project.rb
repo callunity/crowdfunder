0600216793772b4073b8517acc9ba35adfce04f8
callunity <heather.e.armstrong@gmail.com>
2014-09-24 15:31:01 -0400

    Changed 'donations' to 'pledges' in models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 
   has_many :tiers
   accepts_nested_attributes_for :tiers, reject_if: :all_blank, allow_destroy: true
-  has_many :donations, through: :tiers
-  has_many :backers, through: :donations, class_name: "User"
+  has_many :pledges, through: :tiers
+  has_many :backers, through: :pledges, class_name: "User"
   belongs_to :creator, class_name: "User"
 end
--- app/models/tier.rb
+++ app/models/tier.rb
@@ -2,5 +2,5 @@ class Tier < ActiveRecord::Base
 
   belongs_to :project
   has_many :pledges
-  has_many :backers, through: :donations, class_name: "User"
+  has_many :backers, through: :pledges, class_name: "User"
 end
