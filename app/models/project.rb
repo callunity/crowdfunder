504afa7fd80a34154cbdec3ca9f957b25e369fef
Andrew Smith <smithad15@gmail.com>
2014-09-24 15:20:49 -0400

    Change validation order for error display

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,6 @@ class Project < ActiveRecord::Base
   has_many :backers, through: :donations, class_name: "User"
   belongs_to :creator, class_name: "User"
 
-  validates :goal, :end_date, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :goal, :end_date, presence: true
 end
