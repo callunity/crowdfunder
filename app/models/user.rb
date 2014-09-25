58be19e644cbb296b4b59632b544ed02190e62cd
callunity <heather.e.armstrong@gmail.com>
2014-09-25 17:39:16 -0400

    Users can view projects that a user has backed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ActiveRecord::Base
   has_many :pledges, foreign_key: 'backer_id'
   has_many :backed_projects, through: :pledges, source: :project
 
+  def total_money
+    self.pledges.inject(0) { |sum, pledge| sum + pledge.amount }
+  end
+
+  def successful_backed_projects
+    self.backed_projects.select { |project| project.end_date < Time.now }
+  end
+
+  def unexpired_projects
+    self.backed_projects.select { |project| project.end_date > Time.now }
+  end
+
 end
