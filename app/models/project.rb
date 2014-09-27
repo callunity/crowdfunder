e9853a18978d9dcf128d5261c3113eb4a8d9d99a
callunity <heather.e.armstrong@gmail.com>
2014-09-27 14:04:12 -0400

    Previous date display model because to_date(:long_ordinal) isn't working with this particular date selection method for new projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -28,11 +28,11 @@ class Project < ActiveRecord::Base
   end
 
   def end_date_display
-    self.end_date.to_date.to_s(:long_ordinal)
+    self.end_date.to_time.strftime('%A, %B %e, %Y')
   end
 
   def start_date_display
-    self.start_date.to_date.to_s(:long_ordinal)
+    self.start_date.to_time.strftime('%A, %B %e, %Y')
   end
 
 end
