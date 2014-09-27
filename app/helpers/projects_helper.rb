1432a84556bdcd878ae9eb4d308c73117b36c580
Andrew Smith <smithad15@gmail.com>
2014-09-27 15:33:45 -0400

    Fix date helper methods

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,7 @@
 module ProjectsHelper
 
-	def end_date_display
-    self.end_date.to_time.strftime('%A, %B %e, %Y')
-  end
-
-  def start_date_display
-    self.start_date.to_time.strftime('%A, %B %e, %Y')
+	def date_display(date)
+    date.to_time.strftime('%A, %B %e, %Y')
   end
 
 end
