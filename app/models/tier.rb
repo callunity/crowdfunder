cf9d497512beea6407cc899092345a8eb9bd77de
Andrew Smith <smithad15@gmail.com>
2014-09-27 15:16:29 -0400

    Change from source option to class_name

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -2,5 +2,5 @@ class Tier < ActiveRecord::Base
 
   belongs_to :project
   has_many :pledges
-  has_many :backers, through: :pledges, source: :user, foreign_key: 'backer_id'
+  has_many :backers, through: :pledges, class_name: "User", foreign_key: 'backer_id'
 end
